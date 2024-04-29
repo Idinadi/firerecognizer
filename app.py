aac946262cec72b37eb7521ee64feb794a75a171
Idinadi <92621722+Idinadi@users.noreply.github.com>
2024-04-29 20:39:46 +0530

    version 1.0
    Added Files for the Fire Recognition

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import json, cv2
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the model
+model = pickle.load(open('model.pkl','rb'))
+sift = cv2.SIFT_create()
+min_length = 768
+
+def preprocess(text):
+    image = cv2.imread(text)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    keypoints, descriptors = sift.detectAndCompute(gray, None)
+    final_features = descriptors.flatten()
+    X_uniform = [final_features[:min_length]]
+    return X_uniform
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    processed = preprocess(data['text'])
+    preds = model.predict_proba(processed)[0]
+    print(preds)
+    if preds[0] > preds[1]:
+        return jsonify({"message" : "Negative", "pred": preds[0]})
+    return jsonify({"message" : "Fire Detected", "pred": preds[1]})
+
+if __name__=="__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
